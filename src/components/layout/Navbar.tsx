2863a7d0b1bd368e45cb734f9bbca6f81f728bb5
Yassar_Ymamou <115738354+Faka2005@users.noreply.github.com>
2025-10-10 09:24:59 +0000

    test1

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,17 @@
-
+import { Link } from "react-router-dom";
 function Navbar(){
     return 
     (
-        <h1>bonjour</h1>
+        <nav>
+            <ul>
+                <li>
+                    <Link to={'/login'}>Se connecter</Link>
+                </li>
+                <li>
+                    <Link to={'/signup'}>S'inscrire</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 

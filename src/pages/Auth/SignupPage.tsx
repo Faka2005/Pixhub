7dfe0a03f9f479e2fb7ef781f13da8d55ee5ee0f
Yassar_Ymamou <115738354+Faka2005@users.noreply.github.com>
2025-10-10 10:08:09 +0200

    réglages2

--- src/pages/Auth/SignupPage.tsx
+++ src/pages/Auth/SignupPage.tsx
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
+
 
 function SignupPage() {
 
-        const [email,setEmail]= useState('');
-        const [password,setPassword]= useState('');
-        const [erreur,setErreur]= useState('');
-        const [ErreurName,setErreurName]= useState('');
-        const [erreurEmail,setErreurEmail]= useState('');
-        const [ErreurPassword,setErreurPassword]= useState('');
+        
   return (
     <>
-    <form action="" method="get" return false >
-      <input type="text" name="name" id="name" onChange={e => setName(e.target.value)}/>
-      <input type="email" name="email" id="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" name="password" id="password" placeholder="Mot de passe" onChange={e => setPassword(e.target.value)}/>
+    <form action="" method="get" >
+      <input type="text" name="name" id="name" />
+      <input type="email" name="email" id="email" placeholder="Email" />
+      <input type="password" name="password" id="password" placeholder="Mot de passe" />
       <button type="submit">S'inscrire'</button>
     </form>
-    <p>{erreur}</p>
+    
     </>
   );
 }
